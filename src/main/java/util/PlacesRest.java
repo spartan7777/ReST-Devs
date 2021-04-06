1c03687b14fef5f5b1394ad123daafb33a56bc02
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-06 12:45:41 -0500

    added method to get places based on an industry id

--- src/main/java/util/PlacesRest.java
+++ src/main/java/util/PlacesRest.java
@@ -3,6 +3,7 @@ package util;
 import java.util.*;
 
 import matc.edu.entity.IndustryDataItem;
+import matc.edu.entity.Place;
 import matc.edu.entity.PlaceDataItem;
 import matc.edu.entity.Industry;
 import org.apache.logging.log4j.LogManager;
@@ -16,4 +17,25 @@ import javax.ws.rs.core.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PlacesRest {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private final String tempIndustry = "5415";
+    public PlacesRest() {
+
+    }
+
+    public List<PlaceDataItem> getPlaces() throws Exception {
+        String targetString =
+                "https://datausa.io/api/data?PUMS%20Industry=" +
+                tempIndustry +
+                "&drilldowns=PUMA&measure=Total%20Population,ygipop%20RCA,Record%20Count&Record%20Count>=5";
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target(targetString);
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        Place resultList = mapper.readValue(response, Place.class);
+        logger.info(resultList.getData());
+        return resultList.getData();
+    }
+
 }
