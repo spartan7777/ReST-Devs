635974d7829900c67ec47934714672af241f8866
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-06 12:39:03 -0500

    added anticipated imports

--- src/main/java/util/PlacesRest.java
+++ src/main/java/util/PlacesRest.java
@@ -1,4 +1,19 @@
 package util;
 
+import java.util.*;
+
+import matc.edu.entity.IndustryDataItem;
+import matc.edu.entity.PlaceDataItem;
+import matc.edu.entity.Industry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class PlacesRest {
 }
