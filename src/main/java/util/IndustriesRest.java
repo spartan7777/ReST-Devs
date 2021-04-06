624f1e0398cda35f8556bb353d9bce4fa9b5727c
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-06 10:15:17 -0500

    added some preliminary templating for REST classes

---
+++ src/main/java/util/IndustriesRest.java
@@ -0,0 +1,10 @@
+package util;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IndustriesRest {
+}
