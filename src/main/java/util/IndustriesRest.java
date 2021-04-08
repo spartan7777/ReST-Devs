50bd0f4f4b6e76225429a8e6add759855c14d517
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-08 08:17:51 -0500

    added javadocs and relevant comments

--- src/main/java/util/IndustriesRest.java
+++ src/main/java/util/IndustriesRest.java
@@ -18,15 +18,27 @@ import javax.ws.rs.Produces;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.parser.JSONParser;
 
+/**
+ * This class contains the methods for retrieving, formatting, and returning industry data for our service
+ * @author Robert Adams
+ */
 public class IndustriesRest {
 
     private List<IndustryDataItem> industryData;
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    /**
+     * Constructor
+     */
     public IndustriesRest() {
 
     }
 
+
+    /**
+     * Gets industries from consumed service. Sets the class instance variable with the result.
+     * @throws Exception
+     */
     public void getIndustries() throws Exception {
         String targetString = "https://datausa.io/api/data?measure=Average%20Wage&drilldowns=Industry%20Group&Year=2019";
         Client client = ClientBuilder.newClient();
@@ -38,13 +50,23 @@ public class IndustriesRest {
         industryData = resultList.getData();
     }
 
+    /**
+     * Formats consumed data into friendly, sorted JSON
+     * @return
+     */
     private JSONArray putIndustryNameIdAndAvgWageIntoJSON() {
         JSONArray sortedJSON = null;
         try {
-            getIndustries(); //method gets all industries and puts them in instance variable industryData
-            Set<String> sortedSet = new TreeSet<>(); //TreeSet used for natural-order sorting
+
+            //gets all industries and puts them in instance variable industryData
+            getIndustries();
+
+            //TreeSet used for natural-order sorting
+            Set<String> sortedSet = new TreeSet<>();
+
             for (IndustryDataItem industry : industryData) {
-                //Create string containing our data to be converted into JSON
+                //Create string containing our data to be converted into JSON.
+                //TODO clean this up a bit - it can probably be done better
                 String jsonObjectString = "{"
                         + "\""
                         + industry.getIndustryGroup()
@@ -58,6 +80,9 @@ public class IndustriesRest {
                         + industry.getAverageWage()
                         + "\""
                         + "}}";
+
+                //Puts resulting JSON string into a sorted set - this is for alphabetical sorting
+                //since a TreeSet sorts natural order by default
                 sortedSet.add(jsonObjectString);
             }
             JSONParser parser = new JSONParser();
@@ -70,6 +95,10 @@ public class IndustriesRest {
         return sortedJSON;
     }
 
+    /**
+     * Returns our produced JSON data for REST service
+     * @return
+     */
     @GET
     @Path("/industries")
     @Produces(MediaType.APPLICATION_JSON)
@@ -77,8 +106,4 @@ public class IndustriesRest {
         return putIndustryNameIdAndAvgWageIntoJSON();
     }
 
-    public static void main(String[] args) {
-        IndustriesRest test = new IndustriesRest();
-        test.putIndustryNameIdAndAvgWageIntoJSON();
-    }
 }
