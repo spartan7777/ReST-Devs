9adc9ac9c27a57b6ebc451f169266a256590c4be
Tosan Ogedengbe <togedengbe@madisoncollege.edu>
2021-04-11 07:07:11 -0500

    Updated Test classes.

--- src/test/java/matc/edu/entity/TestServiceClient.java
+++ src/test/java/matc/edu/entity/TestServiceClient.java
@@ -15,29 +15,29 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class TestServiceClient {
 
     @Test
-    public void testIndustryDataItemJSON() throws Exception {
+    public void testIndustryJSON() throws Exception {
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("https://datausa.io/api/data?PUMS%20Industry=INDUSTRY&drilldowns=PUMA&measure=Total%20Population,ygipop%20RCA,Record%20Count");
+                client.target("https://datausa.io/api/data?measure=Average%20Wage&drilldowns=Industry%20Group&Year=2019");
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        IndustryDataItem industry = mapper.readValue(response, IndustryDataItem.class);
-        assertNotNull(industry);
-        assertEquals("?????", industry.getIndustryGroup());
+        Industry industries = mapper.readValue(response, Industry.class);
+        assertNotNull(industries);
+        assertEquals("112", industries.getData().get(1).getiDIndustryGroup());
     }
 
     @Test
-    public void testPlaceDataItemJSON() throws Exception {
+    public void testPlaceJSON() throws Exception {
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("https://datausa.io/api/data?PUMS%20Industry=25&drilldowns=PUMA&measure=Total%20Population,ygipop%20RCA,Record%20Count");
+                client.target("https://datausa.io/api/data?PUMS%20Industry=5415&drilldowns=PUMA&measure=Total%20Population,ygipop%20RCA,Record%20Count");
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        PlaceDataItem place = mapper.readValue(response, PlaceDataItem.class);
-        assertNotNull(place);
-        assertEquals("?????", place.getPUMSIndustry());
+        Place places = mapper.readValue(response, Place.class);
+        assertNotNull(places);
+        assertEquals("5415", places.getData().get(0).getIDPUMSIndustry());
     }
 
 }
